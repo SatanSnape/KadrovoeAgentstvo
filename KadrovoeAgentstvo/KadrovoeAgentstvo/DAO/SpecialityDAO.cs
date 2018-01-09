03a56dd040fac0211cb3d51a2c25c5db8748534d
Anastasiya Krutova <satansnape@gmail.com>
2018-01-09 23:36:12 +0300

    логирование

--- KadrovoeAgentstvo/KadrovoeAgentstvo/DAO/SpecialityDAO.cs
+++ KadrovoeAgentstvo/KadrovoeAgentstvo/DAO/SpecialityDAO.cs
@@ -3,29 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using KadrovoeAgentstvo.Models;
+using NLog;
 
 namespace KadrovoeAgentstvo.DAO
 {
     public class SpecialityDAO
     {
-
+        private static Logger logger = LogManager.GetCurrentClassLogger();
         private KadrovoeAgentstvoEntities _db = new KadrovoeAgentstvoEntities();
 
         public List<Speciality> GetAllSpecilities() => _db.Specialities.ToList();
 
         public void CreateSpeciality(Speciality spec)
         {
+            logger.Debug("Создание новой вакансии");
             var speciality = new Speciality
             {
                 Name = spec.Name
             };
+            logger.Debug("Сохранение новой вакансии");
             _db.Specialities.Add(spec);
             _db.SaveChanges();
         }
 
         public void EditSpeciality(Speciality spec)
         {
+            logger.Debug("Редактирование вакансии");
             var specEntity = _db.Specialities.FirstOrDefault(x => x.SpecialityId == spec.SpecialityId);
+            if (specEntity == null)
+            {
+                logger.Error("Вакансия для редактирования не найдена");
+                throw new Exception("Вакансия для редактирования не найдена");
+            }
             specEntity.Name = spec.Name;
             _db.SaveChanges();
         }
@@ -34,9 +43,13 @@ namespace KadrovoeAgentstvo.DAO
 
         public void RemoveSpeciality(Speciality spec)
         {
-            var entity = _db.Specialities.FirstOrDefault(x => x.SpecialityId == spec.SpecialityId);
-            if (entity != null)
-                _db.Specialities.Remove(entity);
+            var specEntity = _db.Specialities.FirstOrDefault(x => x.SpecialityId == spec.SpecialityId);
+            if (specEntity == null)
+            {
+                logger.Error("Вакансия для удаления не найдена");
+                throw new Exception("Вакансия для удаления не найдена");
+            }
+            _db.Specialities.Remove(specEntity);
             _db.SaveChanges();
         }
 
